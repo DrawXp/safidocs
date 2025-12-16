a37870d4037d057668dd36a1fc73e0a9fc460741
DrawXp <joaobosco5405@gmail.com>
2025-12-16 17:52:05 -0300

    docs update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,7 +52,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/safido.webp',
     colorMode: {
       defaultMode: 'dark',
       disableSwitch: false,
